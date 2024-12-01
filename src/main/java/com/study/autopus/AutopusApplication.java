183a0e0d4b02c8990c673f5b98655817a2bf51f7
chosoobin37 <angelajsb@naver.com>
2024-12-01 17:06:57 +0900

    chore: application setting, dependency add

--- src/main/java/com/study/autopus/AutopusApplication.java
+++ src/main/java/com/study/autopus/AutopusApplication.java
@@ -1,13 +1,20 @@
 package com.study.autopus;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AutopusApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AutopusApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper getModelMapper() {
+        return new ModelMapper();
     }
 
 }
