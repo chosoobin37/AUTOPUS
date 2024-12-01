4bb60fc53d3a2e164cd0ddee885f9fdd0f48b4ef
chosoobin37 <angelajsb@naver.com>
2024-12-01 16:52:28 +0900

    First Commit

---
+++ src/main/java/com/study/autopus/AutopusApplication.java
@@ -0,0 +1,13 @@
+package com.study.autopus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutopusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutopusApplication.class, args);
+    }
+
+}
