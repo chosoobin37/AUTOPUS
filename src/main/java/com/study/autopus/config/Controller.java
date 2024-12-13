d27831b06869c0545705a7cdf0b6be534cf92df0
chosoobin37 <angelajsb@naver.com>
2024-12-13 17:45:33 +0900

    feat: healthcheck

--- src/main/java/com/study/autopus/config/Controller.java
+++ src/main/java/com/study/autopus/config/Controller.java
@@ -1,17 +1,15 @@
 package com.study.autopus.config;
 
 
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/healthcheck")
 public class Controller {
 
-    @GetMapping
+    @GetMapping("/healthcheck")
     public String healthcheck() {
         return "healthcheck";
     }
-
 }
