18062cafdae212bc610afe0787432e8f126b25d7
chosoobin37 <angelajsb@naver.com>
2024-12-13 17:46:12 +0900

    feat: findByMemberMobile create

--- src/main/java/com/study/autopus/login/repository/LoginRepository.java
+++ src/main/java/com/study/autopus/login/repository/LoginRepository.java
@@ -2,7 +2,8 @@ package com.study.autopus.login.repository;
 
 import com.study.autopus.login.aggregate.Login;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface LoginRepository extends JpaRepository<Login, Integer> {
-
+    Optional<Login> findByMemberMobile(String mobile);
 }
