359511185d1f3dbaa7dc9c4707f52b0ce2960102
chosoobin37 <angelajsb@naver.com>
2024-12-13 17:44:47 +0900

    feat: SecurityConfig login, home allow

--- src/main/java/com/study/autopus/config/SecurityConfig.java
+++ src/main/java/com/study/autopus/config/SecurityConfig.java
@@ -5,22 +5,44 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .csrf(csrf -> csrf.disable()) // CSRF 비활성화
+//                .authorizeHttpRequests(auth -> auth
+//                        .anyRequest().permitAll() // 모든 요청 허용
+//                )
+//                .formLogin(form -> form
+//                        .loginPage("/login") // 커스텀 로그인 페이지 설정
+//                        .permitAll() // 로그인 페이지는 인증 없이 허용
+//                );
+//
+//        return http.build();
+//    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // CSRF 비활성화
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll() // 모든 요청 허용
+                        .requestMatchers("/", "/login", "/oauth2/**").permitAll() // 경로 허용
+                        .anyRequest().authenticated() // 나머지 요청은 인증 필요
                 )
-                .formLogin(form -> form
-                        .loginPage("/login") // 커스텀 로그인 페이지 설정
-                        .permitAll() // 로그인 페이지는 인증 없이 허용
+                .oauth2Login(oauth -> oauth
+                        .defaultSuccessUrl("/home") // 로그인 성공 후 이동 경로
+                        .failureUrl("/login?error=true") // 로그인 실패 시 이동 경로
                 );
 
         return http.build();
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
